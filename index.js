a3fddebcefb7e0ea5a648045ed0a16da41fd8301
santiagotbd <santiago.taboada@utec.edu.pe>
2026-05-07 16:02:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function obtenerMensaje() {
 
 function obtenerHora() {
   const ahora = new Date(); //Date
-  return ahora.toLocaleString("es-PE", { timeZone: "America/Lima });
+  return ahora.toLocaleString("es-PE", { timeZone: "America/Lima" });
 }
 
 app.get("/", (req, res) => {
